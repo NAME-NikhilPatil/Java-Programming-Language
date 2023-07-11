317895f23e5c7fdb614f062879839b11f5f77b80
NAME-NikhilPatil <nikhilpatilhaha@gmail.com>
2023-07-11 13:43:49 +0530

    Just staring don't know much :))

---
+++ JavaBasics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+//Type promotion
+public class JavaBasics{
+    public static void main(String args[]){
+     
+      int age=10;
+      if(age>=18){
+        System.out.println("Adult");
+      }else{
+                System.out.println("Not Adult");
+
+      }
+
+    }
+}
